301476d7da0d42ba5f03558d030730ad17714f2f
Adenholm <hanni-01@live.se>
2022-04-08 13:06:11 +0200

    Added a sound to the modal window

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,12 +1,17 @@
 import "./Modal.css"
-import sound from "react-sound";
+import Sound from "react-sound";
 import stitch from "./images/stitch.png"
+import computerKeyboard from "./sounds/Computer-keyboard.wav";
 
 const Modal =({setIsOpen,props}) =>{
     return(
         <div className="modal">
             <button onClick={() => setIsOpen(false)}><img src={props.img} className="bigPhoto"/></button>
-
+            <Sound
+                url = {computerKeyboard}
+                playStatus = {Sound.status.PLAYING}
+                playFromPosition={0}
+            />
         </div>
     );
 };

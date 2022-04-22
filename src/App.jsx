ddcaac9c4f0810331b43cee557b8f71a34c870e1
Lisa <69895254+Zeeta00@users.noreply.github.com>
2022-04-22 11:50:02 +0200

    Cleaner code

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,7 @@
-/*import logo from './logo.svg';
-import './App.css';
-import React from "react"
-import Card from "./Components/Card"
-import stitch from "./Components/images/stitch.png"
-import pettson from "./Components/images/pettsonsocka.jpg"
-import Modal from './Components/Modal.js';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import {
-Container, Row, Col
-} from 'reactstrap';
-
-
-function App() {
-
-  const numberOfCards = 2;
-
-  return(
-    <div className="CardGrid">
-    <Container>
-       <Row xs={3}>
-       {[...Array(numberOfCards)].map((e, i) => {
-           return (
-             <Col>
-             <Card 
-             img = {pettson}
-             />
-             </Col>
-           )
-       })}
-       </Row>
-   </Container>
-</div>
-  )
-}
-
-export default App;*/
 
 
 import React from 'react';
-import Image from './Components/Image';
+
 import data from './Components/data.json';
 import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
 import stitch from "./Components/images/stitch.png"
@@ -50,8 +11,7 @@ import Card from "./Components/Card"
 
 
 function createImage(image) {
-  console.log(image);
-// return <Image source={image} key={image} />;
+
   return <Card img = {image} key={image} />;
   
 }

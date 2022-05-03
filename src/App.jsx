7877c79b1413addbad9dec668cbc39c9921e409e
Saaghar <saghar94a@gmail.com>
2022-05-03 12:57:29 +0200

    added sound and close button

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 
 import data from './Components/data.json';
-import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
+//import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
 import stitch from "./Components/images/stitch.png"
 import pettson from "./Components/images/pettsonsocka.jpg"
 import Card from "./Components/Card"
@@ -16,15 +16,20 @@ function createImage(image) {
   
 }
 
+
 function createImages(images) {
   return images.map(createImage);
 }
+
+
 
 function App() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-12 text-center">
+        
+
 
           
           
--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -4,14 +4,31 @@ import stitch from "./images/stitch.png";
 import pettson from "./images/pettsonsocka.jpg";
 import computerKeyboard from "./sounds/Computer-keyboard.wav";
 import cat from "./sounds/cat.wav";
+import closeicon from './icons/cross.png';
+import {useState} from 'react';
+
+function clickMe(){
+  alert('You clicked me!')
+}
+//<button className="closeBtn" onClick={() => setIsOpen(false)}><img src={closeicon} className="closeImg"/></button>  
+//<img src={closeicon} onClick={() => setIsOpen(false)} className="closeImg"/>
+
+//img src={props.img} className="bigPhoto"
 
 const Modal =({setIsOpen,props}) =>{
+    const [isPlaying,setIsPlaying] = useState(true);
     return(
         <div id="modal">
-            <button className= "btn" onClick={() => setIsOpen(false)}><img src={props.img} className="bigPhoto"/></button>
+            <img src={closeicon} className="closeImg" onClick={() => setIsOpen(false)}/>
+
+            
+
+            <button className= "btn" onClick={() => setIsPlaying(!isPlaying)} ><img src={props.img} className="bigPhoto"/></button>
             <Sound
                 url = {cat}
-                playStatus = {Sound.status.PLAYING}
+                playStatus = {
+                    Sound.status.PLAYING
+                }
                 playFromPosition={0}
             />
         </div>

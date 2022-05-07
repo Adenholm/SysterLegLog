39c3b2cf656cc59b7f3405e2c075177047097a2b
Adenholm <hanni-01@live.se>
2022-05-08 00:37:45 +0200

    Created grid with four random cards

--- src/Components/pages/GuessPage.js
+++ src/Components/pages/GuessPage.js
@@ -1,8 +1,57 @@
-import React from "react";
+import React ,{useEffect, useState } from "react"
+import axios from 'axios'
+import Card from "./GuessCard"
+import Sound from "react-sound"
+
+function GuessPage (props){
+    const [cards, setCards] = useState([])
+    const [correctCard, setCorrectCard] = useState(undefined)
+    const [isPlaying,setIsPlaying] = useState(false);
+    const numberOfCards = 4
+  
+    useEffect(() => {
+      fetchCards()
+    },[])
+  
+    const fetchCards = async () => {
+      //send get request to cards/all
+      axios
+        .get('http://localhost:4001/cards/all')
+        .then(response => {
+          console.log(response.data)
+          setCards(response.data.sort(() => Math.random() - Math.random()).slice(0, numberOfCards))
+          setCorrectCard(cards[Math.floor(Math.random() * cards.length)])
+          setIsPlaying(true)
+        })
+        .catch(error => console.error('Couldnt retrieve cards: ' + error))
+    }
 
 
-function GuessPage (){
-
+    
+    
+    return (
+      <div className="row">
+             {createCards(cards)} 
+             <Sound
+                url = {correctCard ? correctCard.Sound : ''}
+                playStatus = {
+                  isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED
+                }
+                playFromPosition={0}
+            />
+      </div>
+      );
+  
 }
+
+
+function createCards(images) {
+    return images.map(cardData => {return <Card 
+        img = {cardData.pic} 
+        isCorrect = {cardData.isCorrect}
+        />;});
+}
+
+
 
 export default GuessPage;

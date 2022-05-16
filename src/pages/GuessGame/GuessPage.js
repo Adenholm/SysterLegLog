382a67c1ce4fac3f399b0171a029ab5e432563f9
Lisa <69895254+Zeeta00@users.noreply.github.com>
2022-05-16 11:20:01 +0200

    added images and some css

--- src/pages/GuessGame/GuessPage.js
+++ src/pages/GuessGame/GuessPage.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "./GuessCard";
+import confetti from "../../Components/icons/confetti.gif";
+import rightArrow from "../../Components/icons/right-arrow.png";
+import leftArrow from "../../Components/icons/left-arrow.png";
 import Sound from "react-sound";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import "./GuessPage.css";
 
 function GuessPage(props) {
   const location = useLocation();
@@ -45,24 +49,27 @@ function GuessPage(props) {
 
   return (
     <div>
-      <button>
-        <Link to={-1}>Tillbaka</Link>
+      <button className="btnBack">
+        <img src={leftArrow} className="btnBackImage" />
+        <Link to={-1} className="btnBackLink">
+          Tillbaka till startsidan
+        </Link>
       </button>
 
-      <button onClick={fetchCards}>Nästa</button>
-      <div className="row">
+      <div className="allCards">
         {createCards(cards)}
         <Sound
           url={correctCard ? correctCard.sound : ""}
           playStatus={isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED}
           playFromPosition={0}
         />
-
-        <button className="btn" onClick={() => setIsPlaying(!isPlaying)}>
-          Spela igen
+        <img src={rightArrow} onClick={fetchCards} className="btnNext" />
+        <button className="btnReplay" onClick={() => setIsPlaying(!isPlaying)}>
+          Lyssna igen
         </button>
+
+        <img src={confetti} className="confetti" />
       </div>
-      <button onClick={fetchCards}>Nästa</button>
     </div>
   );
 }

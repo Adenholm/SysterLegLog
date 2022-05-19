58d17ace84cdc17c0a2b257a23cff9188ccd756f
Lisa <69895254+Zeeta00@users.noreply.github.com>
2022-05-19 09:25:25 +0200

    Font classy

--- src/pages/Categories/CategoryCard.js
+++ src/pages/Categories/CategoryCard.js
@@ -1,24 +1,27 @@
 import React from "react";
-import "./CategoryCard.css"
+import "./CategoryCard.css";
 import { useState } from "react";
-import {Link, useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 const CategoryCard = (props) => {
-    const navigate = useNavigate();
-    const toCardPage = () => {
-        navigate('/Cards',{state:{category:props.title}});
-    }
+  const navigate = useNavigate();
+  const toCardPage = () => {
+    navigate("/Cards", { state: { category: props.title } });
+  };
 
-    return(
-        
-            <div onClick={()=>{toCardPage()}} className="cardBody">
-                
-                <button className = "cardBtn">
-                    <img src={props.img} className= "photo"/> 
-                        <h2><b>{props.title}</b></h2>
-                </button> 
-            </div>
-
-        
-    );
+  return (
+    <div
+      onClick={() => {
+        toCardPage();
+      }}
+      className="cardBody"
+    >
+      <button className="cardBtn">
+        <img src={props.img} className="photo" />
+        <h2 className="categoryTitletxt">
+          <b>{props.title}</b>
+        </h2>
+      </button>
+    </div>
+  );
 };
 export default CategoryCard;
--- src/pages/GuessGame/GuessSettings.js
+++ src/pages/GuessGame/GuessSettings.js
@@ -1,73 +1,79 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import DiscreteSlider from "./NumberCardsButton";
 import Checkbox from "../../Components/Checkbox";
 import { Categories } from "../../Components/Categories";
 
 //array that doesn't include the 'Alla kategorier' category
-var onlyCategories = Categories.slice(1)
+var onlyCategories = Categories.slice(1);
 
-function GuessSettings (){
-    const [numberOfCards, setNumberOfCards] = useState(4)
-    const navigate = useNavigate();
+function GuessSettings() {
+  const [numberOfCards, setNumberOfCards] = useState(4);
+  const navigate = useNavigate();
 
-    //array with individual states for each checkbox to keep track of which checkboxes are checked or not
-    const states = []
-    //array with state for only the 'Alla kategorier'-checkbox (actually just a single value)
-    const allCategoryState = []
-    //var that keeps track on where each category has its state
-    var i = -1;
-    //function for creating the 'Alla kategorier'-checkbox
-    function createAllCatCheckbox(cardData){
-        return (
-            <Checkbox
-                id = "checkbox"
-                label = {cardData.name}
-                index = {0}
-                getState = {(state,index) => allCategoryState[index]= state}
-                />
-        )
-    }
-    //function for creating remaining checkboxes
-    function createCheckbox(cardData) {
-        i++;
-        return(    
-                <Checkbox 
-                id = "checkbox"
-                label = {cardData.name}
-                index = {i}
-                getState = {(state,index) => states[index]=state}
-                />
-        )
-    }
+  //array with individual states for each checkbox to keep track of which checkboxes are checked or not
+  const states = [];
+  //array with state for only the 'Alla kategorier'-checkbox (actually just a single value)
+  const allCategoryState = [];
+  //var that keeps track on where each category has its state
+  var i = -1;
+  //function for creating the 'Alla kategorier'-checkbox
+  function createAllCatCheckbox(cardData) {
+    return (
+      <Checkbox
+        id="checkbox"
+        label={cardData.name}
+        index={0}
+        getState={(state, index) => (allCategoryState[index] = state)}
+      />
+    );
+  }
+  //function for creating remaining checkboxes
+  function createCheckbox(cardData) {
+    i++;
+    return (
+      <Checkbox
+        id="checkbox"
+        label={cardData.name}
+        index={i}
+        getState={(state, index) => (states[index] = state)}
+      />
+    );
+  }
 
-    const toGuessPage = () => {
-        navigate('/GuessPage',{
-            state:{
-                categoryStates:states,
-                allCategories: allCategoryState[0],
-                numCards: numberOfCards
-            }});
-    }
-    return(
-        <div>
-            <button><Link to = {-1}>Tillbaka</Link></button>
-            {createAllCatCheckbox(Categories[0])}
-            {onlyCategories.map(createCheckbox)}
-            
-            <li>
-                <DiscreteSlider 
-                    onChangeHandler = {setNumberOfCards} 
-                />
-            </li>
-            <li>
-            <button onClick={() => toGuessPage()}>Spela</button>
-            </li>
-            <button onClick={() => console.log(numberOfCards)}>Debug</button>
-        </div>
-    )
+  const toGuessPage = () => {
+    navigate("/GuessPage", {
+      state: {
+        categoryStates: states,
+        allCategories: allCategoryState[0],
+        numCards: numberOfCards,
+      },
+    });
+  };
+  return (
+    <div className="settings">
+      <button className="btnSettings">
+        <Link to={-1}>Tillbaka</Link>
+      </button>
+      {createAllCatCheckbox(Categories[0])}
+      {onlyCategories.map(createCheckbox)}
 
+      <li>
+        <DiscreteSlider onChangeHandler={setNumberOfCards} />
+      </li>
+      <li>
+        <button className="btnSettings" onClick={() => toGuessPage()}>
+          Spela
+        </button>
+      </li>
+      <button
+        className="btnSettings"
+        onClick={() => console.log(numberOfCards)}
+      >
+        Debug
+      </button>
+    </div>
+  );
 }
 
 export default GuessSettings;
-

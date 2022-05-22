cb4a551d9374cc4e6953ea4e30fcfef700884d91
Adenholm <hanni-01@live.se>
2022-05-22 14:41:10 +0200

    doesnt work yet

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../Pages.css";
+import doggie from "../Components/images/doggie.jpg"
 
 function StartPage() {
   return (
     <div>
+      <img src={doggie} className= "startImage"/>
       <h1 className="text">LegLoggen</h1>
       <ul className="nav">
         <li className="item">
